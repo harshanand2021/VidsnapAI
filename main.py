fe4f68b123c8022f2466266b5a1b21fb27fe3379
Harsh Anand <harsh25061999@gmail.com>
2025-08-15 16:53:51 +0530

    VidsnapAI flask project initialized

---
+++ main.py
@@ -0,0 +1,38 @@
+from flask import Flask,flash, request,redirect, url_for, render_template
+from werkzeug.utils import secure_filename
+import uuid
+import os
+
+UPLOAD_FOLDER = '/user_uploads'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/create", methods=['GET', 'POST'])
+def create():
+    myid = uuid.uuid1()
+    if request.method == 'POST':
+        print(request.files.keys())
+        rec_id = request.form.get("uuid")
+        desc = request.form.get("text")
+        for key, value in request.files.items():
+            print(key,value)
+            
+            # Upload the file
+            file = request.files[key]
+            if file:
+                filename = secure_filename(file.filename)
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename, rec_id, filename))
+            
+    return render_template("create.html", myid=myid)
+
+@app.route("/gallery")
+def gallery():
+    return render_template("gallery.html")
+
+app.run(debug=True)
